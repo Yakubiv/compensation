81aaf33701209e1ccf9c103dbe819f469a0fcc43
Petro <petro.yakubiv@gmail.com>
2017-12-03 12:42:00 +0200

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'calculations#index'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :calculations, only: :index
 end
